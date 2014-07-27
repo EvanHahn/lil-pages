f23c26a46d80e6f1246f943a3988c9dd6a29c95e
Evan Hahn <me@evanhahn.com>
2014-07-26 22:26:09 -0700

    update canvas to favicon

--- js/canvas-to-favicon.js
+++ js/canvas-to-favicon.js
@@ -1,17 +1,60 @@
-;(function(global) {
-var appended = false;
-var link = document.createElement('link');
-link.rel = 'icon';
-link.type = 'image/png';
-function canvasToFavicon(canvas) {
-link.href = canvas.toDataURL('image/png');
-if (!appended) {
-document.head.appendChild(link);
-appended = true;
-}
-}
-if (global.module && module.exports)
-module.exports = canvasToFavicon;
-else
-global.canvasToFavicon = canvasToFavicon;
-})(this);
+/* global document, navigator */
+
+(function() {
+
+  var canvasToFavicon;
+  var supportsCanvas = !!document.createElement('canvas').getContext;
+
+  if (supportsCanvas) {
+
+    var head = document.head;
+
+    var createIconElement = function() {
+      var link = document.createElement('link');
+      link.rel = 'icon';
+      link.type = 'image/png';
+      return link;
+    };
+
+    var isFirefox = !!navigator.userAgent.match(/firefox/i);
+
+    var iconElement;
+
+    canvasToFavicon = function(canvas) {
+
+      if (!iconElement) {
+
+        var existingIcons = document.querySelectorAll('link[rel="icon"]');
+        for (var i = 0, len = existingIcons.length; i < len; i ++) {
+          head.removeChild(existingIcons[i]);
+        }
+
+        iconElement = createIconElement();
+        head.appendChild(iconElement);
+
+      }
+
+      // firefox needs to swap out the old element
+      if (isFirefox) {
+        var newEl = createIconElement();
+        head.replaceChild(newEl, iconElement);
+        iconElement = newEl;
+      }
+
+      iconElement.href = canvas.toDataURL('image/png');
+
+    };
+
+  } else {
+
+    // noop without support
+    canvasToFavicon = function() {};
+
+  }
+
+  if (typeof module !== 'undefined')
+    module.exports = canvasToFavicon;
+  else
+    this.canvasToFavicon = canvasToFavicon;
+
+})();
